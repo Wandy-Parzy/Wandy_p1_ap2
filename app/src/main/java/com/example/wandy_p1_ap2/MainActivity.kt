1bc06e89c2d73914d8900c5a8121996e1726ff0e
Wandy-Parzy <wandypaulino05@gmail.com>
2023-10-01 21:46:08 -0400

    second commit

--- app/src/main/java/com/example/wandy_p1_ap2/MainActivity.kt
+++ app/src/main/java/com/example/wandy_p1_ap2/MainActivity.kt
@@ -8,10 +8,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.wandy_p1_ap2.ui.counter.CounterScreen
+import com.example.wandy_p1_ap2.ui.counter.CounterViewModel
 import com.example.wandy_p1_ap2.ui.theme.Wandy_p1_ap2Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +29,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val viewModel: CounterViewModel =  hiltViewModel()
+                    val counter by viewModel.counter.collectAsState(0)
+                    CounterScreen(
+                        counter = counter,
+                        onIncrement = viewModel::increment
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Wandy_p1_ap2Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/wandy_p1_ap2/Wandy_p1_ap2.kt
@@ -0,0 +1,7 @@
+package com.example.wandy_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+@HiltAndroidApp
+class Wandy_p1_ap2: Application(){
+}
